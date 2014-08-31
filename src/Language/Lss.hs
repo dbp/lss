3b28917ac94a73ffb146c60a10ff190caa7e6f5d
Daniel Patterson <dbp@dbpmail.net>
2014-08-31 14:46:59 -0400

    Draft of parsing added.

--- src/Language/Lss.hs
+++ src/Language/Lss.hs
@@ -3,16 +3,22 @@
 
 module Language.Lss where
 
-import           Data.Map.Strict     (Map)
-import qualified Data.Map.Strict     as M
+import           Control.Applicative  ((<$>), (<*>))
+import           Control.Arrow        ((&&&))
+import qualified Data.Attoparsec.Text as A
+import           Data.List            (partition)
+import           Data.Map.Strict      (Map)
+import qualified Data.Map.Strict      as M
 import           Data.Monoid
-import           Data.Text           (Text)
-import qualified Data.Text           as T
-import qualified Language.Css.Parse  as P
-import qualified Language.Css.Pretty as C
-import qualified Language.Css.Syntax as C
-import           Prelude             hiding ((++))
-import qualified Text.XmlHtml        as X
+import           Data.Text            (Text)
+import qualified Data.Text            as T
+import qualified Language.Css.Parse   as P
+import qualified Language.Css.Pretty  as C
+import qualified Language.Css.Syntax  as C
+import           Prelude              hiding ((++))
+import qualified Text.XmlHtml         as X
+
+import           Debug.Trace
 
 (++) :: Monoid d => d -> d -> d
 (++) = mappend
@@ -24,17 +30,50 @@ deriving instance Ord C.Ident
 unIdent :: C.Ident -> Text
 unIdent (C.Ident s) = T.pack s
 
-data LssState = LssState { sConstants :: ConstMap, sFunctions ::  Map C.Ident LssFunc}
+data LssState = LssState { sConstants :: ConstMap, sFunctions ::  Map C.Ident LssFunc} deriving Show
 
-data LssFunc = LssFunc { fParams :: [C.Ident], fLocalConstants :: ConstMap, fBody :: [C.RuleSet] }
+data LssFunc = LssFunc { fParams :: [C.Ident], fLocalConstants :: ConstMap, fBody :: [C.RuleSet] } deriving Show
 
 instance Monoid LssState where
   mempty = LssState M.empty M.empty
   mappend (LssState c1 f1) (LssState c2 f2) = LssState (M.union c2 c1) (M.union f2 f1)
 
 
-parse :: Text -> Either Text LssState
-parse = undefined
+data FunOrConstOrRule = Fun { unFun :: (C.Ident, LssFunc) }
+                      | Const { unConst :: (C.Ident, C.Expr) }
+                      | Rule { unRule :: C.RuleSet }
+                      deriving Show
+isConst (Const _) = True
+isConst _ = False
+
+parse :: Text -> Either String LssState
+parse = A.parseOnly lssGrammar
+  where lssGrammar = do res <- A.many' (A.choice [A.skipSpace >> fun, A.skipSpace >> parseCon])
+                        traceShow res (return ())
+                        let (cons, funs) = partition isConst res
+                        return (LssState (M.fromList $ map unConst cons) (M.fromList $ map unFun funs))
+        fun = do ident <- P.identp
+                 params <- A.option [] $ do
+                   A.char '('
+                   ps <- A.option [] (do param <- P.identp
+                                         rest <- A.many' $ A.char ',' >> A.skipSpace >> P.identp
+                                         return (param:rest))
+                   A.char ')'
+                   return ps
+                 A.skipSpace
+                 A.char '{'
+                 res <- A.many' (A.choice [Rule <$> (A.skipSpace >> P.rulesetp), A.skipSpace >> parseCon])
+                 let (cons, rules) = partition isConst res
+                 A.skipSpace
+                 A.char '}'
+                 return $ Fun (ident, LssFunc params (M.fromList $ map unConst cons) (map unRule rules))
+
+parseCon = do ident <- P.identp
+              A.skipSpace
+              A.char '='
+              A.skipSpace
+              expr <- P.exprp
+              return $ Const (ident, expr)
 
 apply :: LssState -> C.Ident -> [C.Expr] -> Either Text [C.RuleSet]
 apply state ident args = case M.lookup ident (sFunctions state) of
